ddbea6a5c96856e4bb7cfeefe4167f4a12a4ef41
Dmitry Dyachkov <dimos-d@yandex.ru>
2017-07-24 18:24:00 +0300

    Debug mode and webhook check

--- main.go
+++ main.go
@@ -4,14 +4,16 @@ package main
 
 import (
 	"log"
-	"github.com/go-telegram-bot-api/telegram-bot-api"
 	"net/http"
+
+	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 const (
-	Token = ""
+	IsDebug   = true
+	Token     = ""
 	ServerUrl = "https://www.google.com/"
-	Address = "0.0.0.0:443"
+	Address   = "0.0.0.0:443"
 )
 
 func main() {
@@ -22,25 +24,40 @@ func main() {
 
 	me, _ := bot.GetMe()
 
-	log.Println(me.FirstName)
-	log.Println(me.LastName)
-	log.Println(me.UserName)
-	log.Println(me.LanguageCode)
-	log.Println(me.ID)
+	bot.Debug = IsDebug
 
-	bot.Debug = true
+	if IsDebug {
+
+		DeleteWebHookIfSet(bot)
+
+		log.Println("Info about the bot:")
+		log.Printf("ID: %d", me.ID)
+		log.Printf("FirstName: '%s'", me.FirstName)
+		log.Printf("LastName: '%s'", me.LastName)
+		log.Printf("UserName: '%s'", me.UserName)
+		log.Printf("LanguageCode: '%s'", me.LanguageCode)
+	}
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	_, err = bot.SetWebhook(tgbotapi.NewWebhook(ServerUrl + bot.Token))
+	var updates tgbotapi.UpdatesChannel
 
-	if err != nil {
-		log.Fatal(err)
+	if IsDebug {
+
+		u := tgbotapi.NewUpdate(0)
+		u.Timeout = 10
+		updates, err = bot.GetUpdatesChan(u)
+
+	} else {
+
+		_, err = bot.SetWebhook(tgbotapi.NewWebhook(ServerUrl + bot.Token))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		updates = bot.ListenForWebhook("/" + bot.Token)
+		go http.ListenAndServe(Address, nil)
 	}
-
-	updates := bot.ListenForWebhook("/" + bot.Token)
-
-	go http.ListenAndServe(Address, nil)
 
 	for update := range updates {
 
@@ -48,12 +65,35 @@ func main() {
 			continue
 		}
 
+		log.Println("Found updates")
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 		log.Printf("%+v\n", update)
 
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
 		msg.ReplyToMessageID = update.Message.MessageID
 
+		log.Println("Sending message")
 		bot.Send(msg)
+	}
+}
+
+func DeleteWebHookIfSet(bot *tgbotapi.BotAPI) {
+
+	hook, _ := bot.GetWebhookInfo()
+
+	if !hook.IsSet() {
+		return
+	}
+
+	log.Printf("Found webhook: '%s'", hook.URL)
+	log.Printf("With error: '%s'", hook.LastErrorMessage)
+	log.Printf("Pending updates: %d", hook.PendingUpdateCount)
+	log.Println("Deleting...")
+
+	response, _ := bot.RemoveWebhook()
+	if response.Ok {
+		log.Println("Deleted sucessfully...")
+	} else{
+		log.Printf("Failed to delete with status: %s", response.Description)
 	}
 }
